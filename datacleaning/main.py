e54171cced5f8f7d29d2e212e5daba929fbf4e6a
DanielRocksUrMom <108989218+DanielRocksUrMom@users.noreply.github.com>
2024-02-08 09:43:30 -0500

    Added datasets

--- datacleaning/main.py
+++ datacleaning/main.py
@@ -2,31 +2,12 @@ import pandas as pd
 import math
 import numpy as np
 import csv
+import pandas as pd
 
-data = pd.read_csv("datacleaning\\data\\CSV_Test.csv")
+data = pd.read_csv("datacleaning\\data\\Dataset 1 - Employment Projections by Industry.xlsx - Sheet1.csv")
 
-data.row_
+print(data["Annualized"])
 
-cleaned_data = pd.DataFrame()
-
-counter: int = 0
-
-#with open()
-for i in data.index:
-    row: pd.Series = data.iloc[i]
-    is_valid = True
-    for category in row.keys():
-        try:
-            if math.isnan(float(row[category])):
-                is_valid = False
-                break
-        except:
-            continue
-    if is_valid:
-        pd.concat([])
-        counter += 1
-
-cleaned_data.to_csv("datacleaning\\data\\CSV_Test_cleaned.csv")
     
         
         
