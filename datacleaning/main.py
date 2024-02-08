9a1ee9e820e5fd0706c034930d6c37b3191557e2
DanielRocksUrMom <108989218+DanielRocksUrMom@users.noreply.github.com>
2024-02-08 10:41:43 -0500

    added basic stuff

--- datacleaning/main.py
+++ datacleaning/main.py
@@ -1,12 +1,16 @@
 import pandas as pd
-import math
 import numpy as np
-import csv
 import pandas as pd
 
-data = pd.read_csv("datacleaning\\data\\Dataset 1 - Employment Projections by Industry.xlsx - Sheet1.csv")
+def transform_float_col(input: pd.Series) -> pd.Series:
+    return input.replace(',', '', regex=True).astype('float')
 
-print(data["Annualized"])
+data = pd.read_csv("datacleaning\\data\\Dataset 1 - Employment Projections by Industry.xlsx - Sheet1.csv").replace("*", float('nan'))
+
+data_2021 = data["2021"]
+data_2021 = transform_float_col(data_2021)
+
+print(data_2021.std())
 
     
         
