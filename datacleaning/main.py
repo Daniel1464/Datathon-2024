41d11153e10010279cdfaabad74e264b65dbe744
DanielRocksUrMom <108989218+DanielRocksUrMom@users.noreply.github.com>
2024-02-08 11:10:32 -0500

    More changes

--- datacleaning/main.py
+++ datacleaning/main.py
@@ -1,16 +1,32 @@
 import pandas as pd
 import numpy as np
 import pandas as pd
+import scipy
 
 def transform_float_col(input: pd.Series) -> pd.Series:
     return input.replace(',', '', regex=True).astype('float')
 
-data = pd.read_csv("datacleaning\\data\\Dataset 1 - Employment Projections by Industry.xlsx - Sheet1.csv").replace("*", float('nan'))
+data = pd.read_csv("datacleaning\\data\\GroupedDataset4.csv").replace("*", float('nan'))
 
-data_2021 = data["2021"]
-data_2021 = transform_float_col(data_2021)
+data = data[data['Year'] == 2017]
 
-print(data_2021.std())
+print(data['Enrollment'])
+# means
+#2017: 8215
+#2019: 3739.133333333333
+#2022: 7291.6875
+
+#medians
+#2017: 6487
+#2019: 3176.0
+#2022: 5346.5
+
+
+
+
+
+
+
 
     
         
